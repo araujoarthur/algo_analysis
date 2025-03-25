567e06848affc4c7e8483e627779008e22b43c7c
Arthur Galdino <28057772+araujoarthur@users.noreply.github.com>
2025-03-25 08:57:54 -0300

    some renaming and subtraction completed

--- karatsuba/karatsuba.c
+++ karatsuba/karatsuba.c
@@ -31,8 +31,10 @@ void* terminateNumArray(numarray_t* t) {
 /*
 The summation will not work if they don't have the same amount of digits.
 I present ya: PADDING!
+
+padr pads with zero's to the right of the memory layout
 */
-numarray_t* PadWithZeros(numarray_t* t, size_t howMuch) {
+numarray_t* padr_na(numarray_t* t, size_t howMuch) {
    if (t == NULL) {
       return NULL;
    }
@@ -49,6 +51,45 @@ numarray_t* PadWithZeros(numarray_t* t, size_t howMuch) {
    return t;
 }
 
+// Guess what it does? Pads to the left of the memory layout
+numarray_t* padl_na(numarray_t* t, size_t howMuch) {
+   
+   return NULL;
+}
+
+// Takes a pointer to numarray_t, removes all leading zeros (to the right in memory layout)
+// Returns t without trailling zeros and with the correct length.
+numarray_t* trim_na(numarray_t* t) {
+   if (t == NULL || t->num == NULL) {
+      return NULL;
+   }
+
+   int zeroCount = 0;
+   int lastidx = t->len-1;
+
+   for(int i = t->len-1; i >= 0; i--) {
+      if (t->num[i] != 0) {
+         break;
+      }
+      zeroCount++;
+      lastidx = i;
+   }
+
+   int newlen = t->len - zeroCount;
+   int* old_num = t->num;
+   
+   int* new_num = realloc(t->num, sizeof(int)*newlen);
+   if (new_num == NULL) {
+      return NULL;
+   }
+
+   t->num = new_num;
+   t->len = newlen;
+
+   return t;
+}
+
+// Equalizes the digit length between a and b by adding 0's to the beginning of the one with the smaller length.
 void EqualizePadding(numarray_t* a, numarray_t* b) {
    if (a == NULL || b == NULL) {
       return;
@@ -59,11 +100,11 @@ void EqualizePadding(numarray_t* a, numarray_t* b) {
    }
 
    if(a->len > b->len) {
-      PadWithZeros(b, a->len - b->len);
+      padr_na(b, a->len - b->len);
       return;
    }
 
-   PadWithZeros(a, b->len - a->len);
+   padr_na(a, b->len - a->len);
    return;
 }
 
@@ -253,15 +294,15 @@ void printNumArray(int* numArray, int count) {
 Assumptions: 
    1. A sum of two positive **single-digit** integers is ALWAYS in the interval [0, 18]
 */
-numarray_t* SumNumArrays(numarray_t* a, numarray_t* b) {
+numarray_t* sum_na(numarray_t* a, numarray_t* b) {
    int carry = 0;
    numarray_t* result = createEmptyNumArray(a->len+b->len);
 
    if (a->len != b->len) {
       if (a->len > b->len) {
-         PadWithZeros(b, a->len - b->len);
+         padr_na(b, a->len - b->len);
       } else {
-         PadWithZeros(a, b->len - a->len);
+         padr_na(a, b->len - a->len);
       }
    }
 
@@ -296,11 +337,34 @@ numarray_t* SumNumArrays(numarray_t* a, numarray_t* b) {
 }
 
 // Performs the subtraction a - b (subtracts b from a)
-numarray_t* SubtractNumArray(numarray_t* a, numarray_t* b) {
+// This function has the assumption that a-b is still a positive number.
+numarray_t* subtract_na(numarray_t* a, numarray_t* b) {
    EqualizePadding(a, b);
 
-   numarray_t* result = 
+   numarray_t* result = clone_na(a);
+   int borrow = 0;
+   for(int i = 0; i < a->len; i++) {
+      // Removes the borrowed digit from the previous position.
+      if (borrow > 0) {
+         result->num[i] -= borrow;
+         borrow = 0;
+      }
 
+      // Marks the next field to borrow.
+      if (a->num[i] < b->num[i]) {
+         result->num[i]+=10;
+         borrow++;
+      }
+
+      result->num[i] -= b->num[i];
+   }
+
+   // Checking if after the loop, borrow is still 1. It means something wen't wrong and b > a
+   if (borrow == 1) {
+      exit(EXIT_FAILURE);
+   }
+
+   return trim_na(result);
 }
 
 
@@ -338,8 +402,8 @@ numarray_t* karatsuba(numarray_t* num_a, numarray_t* num_b){
    numarray_t* y0 = y[0];
    numarray_t* y1 = y[1];
 
-   numarray_t* sum_x = SumNumArrays(x[0], x[1]);
-   numarray_t* sum_y = SumNumArrays(y[0], y[1]);
+   numarray_t* sum_x = sum_na(x[0], x[1]);
+   numarray_t* sum_y = sum_na(y[0], y[1]);
 
    numarray_t* z0 = karatsuba(x[0], y[0]);
    numarray_t* z2 = karatsuba(x[1], y[1]);
@@ -348,8 +412,10 @@ numarray_t* karatsuba(numarray_t* num_a, numarray_t* num_b){
    numarray_t* z3 = karatsuba(sum_x, sum_y);
 
    // proceed to subtraction
+   numarray_t* z1 = subtract_na(z3, z2);
+   z1 = subtract_na(z1, z0);
 
-
+   // pad left each zn accordingly, add them, return the result of the addition.
    // should I free num_a and num_b and return only the result?
    return NULL;
 }
@@ -374,7 +440,7 @@ int main() {
    printNumArray(testSum1->num, testSum1->len);
    numarray_t* testSum2 = createArrayFromChar("3");
    printNumArray(testSum2->num, testSum2->len);
-   numarray_t* summation = SumNumArrays(testSum1, testSum2);
+   numarray_t* summation = sum_na(testSum1, testSum2);
    printNumArray(summation->num, summation->len);
    printf("\n\n\n\n");
 
@@ -391,6 +457,65 @@ int main() {
    printNumArray(testPad1->num, testPad1->len);
    printf("Equalized testPad2: ");
    printNumArray(testPad2->num, testPad2->len);
+
+   // TRIM TESTS
+
+   printf("\n\n\n\n");
+   printf("Trim Test Test\n");
+   numarray_t* nothing_to_trim = createArrayFromChar("200");
+   numarray_t* one_zero_to_trim = createArrayFromChar("01000");
+   numarray_t* multiple_zero_to_trim = createArrayFromChar("000103420");
+   printf("nothing_to_trim: ");
+   printNumArray(nothing_to_trim->num, nothing_to_trim->len);
+
+   printf("one_zero_to_trim: ");
+   printNumArray(one_zero_to_trim->num, one_zero_to_trim->len);
+
+   printf("multiple_zero_to_trim: ");
+   printNumArray(multiple_zero_to_trim->num, multiple_zero_to_trim->len);
+
+   printf("\ntrimming\n");
+
+   numarray_t* nothing_to_trim_trimmed = trim_na(nothing_to_trim);
+   numarray_t* one_zero_to_trim_trimmed = trim_na(one_zero_to_trim);
+   numarray_t* multiple_zero_to_trim_trimmed = trim_na(multiple_zero_to_trim);
+
+   printf("nothing_to_trim_trimmed: ");
+   printNumArray(nothing_to_trim_trimmed->num, nothing_to_trim_trimmed->len);
+
+   printf("one_zero_to_trim_trimmed: ");
+   printNumArray(one_zero_to_trim_trimmed->num, one_zero_to_trim_trimmed->len);
+
+   printf("multiple_zero_to_trim_trimmed: ");
+   printNumArray(multiple_zero_to_trim_trimmed->num, multiple_zero_to_trim_trimmed->len);
+
+
+   printf("\n\n\n\n");
+   printf("Subtraction Test\n");
+   numarray_t* testSubtrA = createArrayFromChar("288");
+   printNumArray(testSubtrA->num, testSubtrA->len);
+   numarray_t* testSubtrB = createArrayFromChar("199");
+   printNumArray(testSubtrB->num, testSubtrB->len);
+
+   numarray_t* subtractionAfter = subtract_na(testSubtrA, testSubtrB);
+   printNumArray(subtractionAfter->num, subtractionAfter->len);
+
+   numarray_t* tsA = createArrayFromChar("2888");
+   numarray_t* tsB = createArrayFromChar("999");
+   numarray_t* tsC = createArrayFromChar("100");
+   numarray_t* tsD = createArrayFromChar("10");
+   
+   numarray_t* stepped_subtraction = subtract_na(tsA, tsB);
+   printf("Stepped Subtraction Step 1 (EXPECTED 1889 [9,8,8,1]): ");
+   printNumArray(stepped_subtraction->num, stepped_subtraction->len);
+
+   stepped_subtraction = subtract_na(stepped_subtraction, tsC);
+   printf("Stepped Subtraction Step 2 (EXPECTED 1789 [9,8,7,1]): ");
+   printNumArray(stepped_subtraction->num, stepped_subtraction->len);
+
+   stepped_subtraction = subtract_na(stepped_subtraction, tsD);
+   printf("Stepped Subtraction Step 3 (EXPECTED 1789 [9,7,7,1]): ");
+   printNumArray(stepped_subtraction->num, stepped_subtraction->len);
 
    return 0;
 }
